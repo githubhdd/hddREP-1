c22ca3380fe5a10289ee4b767b0c4478943fd148
hdd <805420784@qq.com>
2019-05-15 19:30:18 +0800

    rep2修改了 Main.java

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		 System.out.println("HelloWorld2!");
+		 System.out.println("rep2ÐÞÐÞ¸ÄÁË!");
 
     }
 }
